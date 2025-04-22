f433ea8f25b603b0787d01f85384660eb9a018a9
saltand <u6a69a9@outlook.com>
2025-04-22 09:08:30 +0800

    feat: 完成组件

---
+++ src/pages/TocItem.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import type { TTocItem } from './index.vue'
+
+const { item, upper, lower } = defineProps<{
+  item: TTocItem
+  upper: number
+  lower: number
+}>()
+
+function getItemOffset(depth: number): number {
+  return depth - 1 <= 0 ? 0 : (depth - 1) * 16
+}
+
+const offset = computed(() => getItemOffset(item.depth))
+const upperOffset = computed(() => {
+  const diff = item.depth - upper
+  if (diff <= 0)
+    return 16
+  else return 0
+})
+const lowerOffset = computed(() => {
+  const diff = item.depth - upper
+  if (diff <= 0)
+    return 0
+  else return 16
+})
+</script>
+
+<template>
+  <a :href="item.link" class="bottom:bt-0 py-2 transition-colors [overflow-wrap:anywhere] relative first:pt-0" :style="{ paddingInlineStart: `${offset}px` }">
+    <svg
+      v-if="item.depth !== upper"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      class="size-4 absolute -top-2"
+      :style="{
+        'inset-inline-start': `${Math.max(offset - 16, 0)}px`,
+      }"
+    >
+      <line :x1="upperOffset" y1="0" :x2="lowerOffset" y2="16" class="stroke-gray-200" strokeWidth="1" />
+    </svg>
+    <div class="bg-gray-200 w-px inset-y-0 absolute z-0" :class="{ 'top-2': item.depth !== upper, 'bottom-2': item.depth !== lower }" />
+    <div class="px-2">
+      {{ item.title }}
+    </div>
+  </a>
+</template>
+
+<style scoped></style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,46 +1,128 @@
-<script setup lang="ts" generic="T extends any, O extends any">
-defineOptions({
-  name: 'IndexPage',
+<script setup lang="ts">
+import { ref, useTemplateRef } from 'vue'
+import TocItem from './TocItem.vue'
+
+export interface TTocItem {
+  title: string
+  link: string
+  depth: number
+}
+
+const tocList = ref<TTocItem[]>([
+  {
+    title: '标题1',
+    link: '#1',
+    depth: 1,
+  },
+  {
+    title: '标题1-1',
+    link: '#1-1',
+    depth: 2,
+  },
+  {
+    title: '标题2',
+    link: '#2',
+    depth: 1,
+  },
+  {
+    title: '标题2-1',
+    link: '#2-1',
+    depth: 2,
+  },
+  {
+    title: '标题2-2',
+    link: '#2-2',
+    depth: 2,
+  },
+  {
+    title: '标题2-2-1',
+    link: '#2-2-1',
+    depth: 3,
+  },
+])
+
+const containerRef = useTemplateRef('containerRef')
+
+const svg = ref<{
+  path: string
+  width: number
+  height: number
+}>({
+  path: '',
+  width: 0,
+  height: 0,
 })
 
-const name = ref('')
+let observer: ResizeObserver | null = null
 
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
+onMounted(() => {
+  if (!containerRef.value)
+    return
+
+  const container = containerRef.value
+  function onResize() {
+    if (container.clientHeight === 0)
+      return
+    let w = 0
+    let h = 0
+    const d: string[] = []
+    for (let i = 0; i < tocList.value.length; i++) {
+      const element: HTMLElement | null = container.querySelector(`a[href="#${tocList.value[i].link.slice(1)}"]`)
+      if (!element)
+        continue
+
+      const styles = getComputedStyle(element)
+      const offset = (tocList.value[i].depth - 1) * 16 + 1
+      const top = element.offsetTop + Number.parseFloat(styles.paddingTop)
+      const bottom = element.offsetTop + element.clientHeight - Number.parseFloat(styles.paddingBottom)
+
+      w = Math.max(offset, w)
+      h = Math.max(h, bottom)
+
+      d.push(`${i === 0 ? 'M' : 'L'}${offset} ${top}`)
+      d.push(`L${offset} ${bottom}`)
+      svg.value = {
+        path: d.join(' '),
+        width: w,
+        height: h,
+      }
+    }
+  }
+  observer = new ResizeObserver(onResize)
+  onResize()
+  observer.observe(container)
+})
+
+onUnmounted(() => {
+  if (observer)
+    observer.disconnect()
+})
+
+const svgMask = computed(
+  () =>
+    `data:image/svg+xml,${encodeURIComponent(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${svg.value.width} ${svg.value.height}"><path d="${svg.value.path}" stroke="black" stroke-width="1" fill="none" /></svg>`)}`,
+)
 </script>
 
 <template>
-  <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu-collective/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
+  <div ref="containerRef" class="min-h-0 relative">
+    <div
+      v-if="svg.height > 0"
+      class="start-4 top-0 absolute z-1"
+      :style="{
+        width: `${svg.width}px`,
+        height: `${svg.height}px`,
+        maskImage: `url(${svgMask})`,
+        WebkitMaskImage: svgMask,
+        maskRepeat: 'no-repeat',
+        WebkitMaskRepeat: 'no-repeat',
+      }"
+    >
+      <div role="none" class="bg-#1f66f4 h-[100px] transition-all" />
+    </div>
 
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button
-        class="text-sm m-3 btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
+    <div class="m-4 flex flex-col">
+      <TocItem v-for="(item, index) in tocList" :key="item.title" :item="item" :upper="tocList[index - 1]?.depth || item.depth" :lower="tocList[index + 1]?.depth || item.depth" />
     </div>
   </div>
 </template>
